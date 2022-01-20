06e0a6c075bda8f54a24bd0ca294605dde6b6d13
Nojus Jankevičius <1nojus.jankevicius@gmail.com>
2022-01-20 08:25:33 +0200

    feat: created JWT after auth

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -1,6 +1,7 @@
 const UserModel = require('../models/user-model');
 const UserViewModel = require('../view-models/user-view-model')
 const { hashPasswordAsync, comparePasswordsAsync } = require('../helpers/hash')
+const generateToken = require('../helpers/generate-token');
 
 const createFakeToken = ({ email, role }) => 'sad.vaas21gsdv.sadfgr2sr';
 
@@ -44,7 +45,7 @@ const signIn = async (req, res) => {
         const user = new UserViewModel(userDoc);
         res.status(200).json({
           user,
-          token: createFakeToken({email, role: userDoc.role}),
+          token: generateToken({email, role: userDoc.role}),
         });
       } else {
         res.status(400).json({
---
+++ server/src/helpers/generate-token.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+const generateToken = ({ email, role }) => {
+  if(email && role) {
+    const token = jwt.sign({ email, role }, process.env.TOKEN_SECRET);
+    return token;
+  }
+  return null;
+};
+
+module.exports = generateToken;
