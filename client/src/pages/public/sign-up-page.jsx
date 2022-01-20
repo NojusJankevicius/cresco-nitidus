12953ddeb5b028d88f3b597adb749e426f062633
Nojus Jankevičius <1nojus.jankevicius@gmail.com>
2022-01-20 16:57:32 +0200

    feat: sign-up page

--- client/src/pages/public/sign-up-page.jsx
+++ client/src/pages/public/sign-up-page.jsx
@@ -1,10 +1,241 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 
-const SignUpPage = () => (
-  <Typography>
-    Sign up
-  </Typography>
-);
+import {
+  CircularProgress,
+  Grid,
+  InputAdornment,
+  TextField,
+} from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ErrorIcon from '@mui/icons-material/Error';
+import AuthForm from '../../components/auth-form';
+import routes from '../../routing/routes';
+import apiService from '../../services/api-service';
+
+const API = {
+  register: () => new Promise((success) => {
+    setTimeout(() => {
+      success({
+        token: 'sdgfisghfsd',
+        user: {
+          name: 'Banys',
+          role: 'Gangster',
+        },
+      });
+    }, 2000);
+  }),
+};
+
+const validationSchema = yup.object({
+  name: yup
+    .string()
+    .required('Privalomas laukas')
+    .min(2, 'Vardui reikalingos mažiausiai dvi raidės')
+    .max(32, 'Vardas negali būti ilgesnis nei 32 raidės')
+    .matches(/^[A-ZĄČĘĖĮŠŲŪŽ]+[a-ząčęėįšųūž]*$/, 'Vardas privalo būti iš didžiosios raidės'),
+  surname: yup
+    .string()
+    .required('Privalomas laukas')
+    .min(2, 'Pavardei reikalingos mažiausiai dvi raidės')
+    .max(32, 'Pavardė negali būti ilgesnis nei 32 raidės')
+    .matches(/^[A-ZĄČĘĖĮŠŲŪŽ]+[a-ząčęėįšųūž]*$/, 'Pavardė privalo būti iš didžiosios raidės'),
+  email: yup
+    .string()
+    .required('Privalomas laukas')
+    .email('Netinkamas pašto formatas')
+    .test('email-validator', 'Paštas negalimas', (_, context) => {
+      const { emailChecked, emailAvailable } = context.parent;
+      if (!emailChecked) return true;
+      return emailAvailable;
+    }),
+  password: yup
+    .string()
+    .required('Privalomas laukas')
+    .min(6, 'Slaptažodžio reikalingi mažiausiai 6 charakteriai')
+    .max(32, 'Slaptažodis negali būti ilgesnis nei 32 charakteriai')
+    .matches(/^.*[A-ZĄČĘĖĮŠŲŪŽ]+.*$/, 'Privalo būti bent viena didžioji raidė')
+    .matches(/^.*\d+.*$/, 'Privalo būti bent vienas skaičius'),
+  passwordConfirmation: yup
+    .string()
+    .required('Privalomas laukas')
+    .oneOf([yup.ref('password')], 'Nesutampa slaptažodžiai'),
+  emailChecked: yup.boolean().oneOf([true]),
+  emailAvailable: yup.boolean().oneOf([true]),
+});
+
+const initialValues = {
+  name: '',
+  surname: '',
+  email: '',
+  password: '',
+  passwordConfirmation: '',
+  emailChecked: false,
+  emailAvailable: false,
+};
+
+const SignUpPage = () => {
+  const [emailCheckLoading, setEmailCheckLoading] = useState(false);
+
+  const onSubmit = async () => {
+    const result = await API.register();
+    console.log(result);
+  };
+
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    errors,
+    touched,
+    values,
+    isSubmitting,
+    isValid,
+    dirty,
+    setFieldValue,
+    setValues,
+  } = useFormik({
+    validateOnMount: true,
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  const handleEmailChange = (e) => {
+    if (values.emailChecked) {
+      setValues({
+        ...values,
+        email: e.target.value,
+        emailChecked: false,
+        emailAvailable: false,
+      }, true);
+    } else {
+      handleChange(e);
+    }
+  };
+
+  const handleEmailBlur = (e) => {
+    handleBlur(e);
+    if (!errors.email) {
+      (async () => {
+        try {
+          setEmailCheckLoading(true);
+          const emailAvailable = await apiService.checkEmail(values.email);
+          setFieldValue('emailAvailable', emailAvailable);
+        } catch (error) {
+          setFieldValue('emailAvailable', false);
+        } finally {
+          setFieldValue('emailCheked', true, true);
+          setEmailCheckLoading(false);
+        }
+      })();
+    }
+  };
+
+  let emailEndorment;
+  if (emailCheckLoading) {
+    emailEndorment = <CircularProgress size={24} />;
+  } else if (!values.emailChecked) {
+    emailEndorment = null;
+  } else if (values.emailAvailable) {
+    emailEndorment = <CheckCircleIcon color="success" />;
+  } else {
+    emailEndorment = <ErrorIcon color="error" />;
+  }
+
+  return (
+    <AuthForm
+      title="Registracija"
+      linkTo={routes.SignInPage}
+      linkTitle="Jau turite paskyrą? Prisijunkite"
+      onSubmit={handleSubmit}
+      isValid={isValid && dirty}
+      loading={isSubmitting}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="name"
+            label="Vardas"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.name}
+            error={touched.name && Boolean(errors.name)}
+            helperText={touched.name && errors.name}
+            disabled={isSubmitting}
+            fullWidth
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="surname"
+            label="Pavardė"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.surname}
+            error={touched.surname && Boolean(errors.surname)}
+            helperText={touched.surname && errors.surname}
+            disabled={isSubmitting}
+            fullWidth
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            name="email"
+            label="El. paštas"
+            onChange={handleEmailChange}
+            onBlur={handleEmailBlur}
+            value={values.email}
+            error={touched.email && Boolean(errors.email)}
+            helperText={touched.email && errors.email}
+            disabled={isSubmitting}
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  {emailEndorment}
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            name="password"
+            label="Slaptažodis"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+            error={touched.password && Boolean(errors.password)}
+            helperText={touched.password && errors.password}
+            disabled={isSubmitting}
+            fullWidth
+            variant="outlined"
+            type="password"
+          />
+        </Grid>
+        <Grid item xs={12} sx={{ mb: 2 }}>
+          <TextField
+            name="passwordConfirmation"
+            label="Pakartokite slaptažodį"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.passwordConfirmation}
+            error={touched.passwordConfirmation && Boolean(errors.passwordConfirmation)}
+            helperText={touched.passwordConfirmation && errors.passwordConfirmation}
+            disabled={isSubmitting}
+            fullWidth
+            variant="outlined"
+            type="password"
+          />
+        </Grid>
+      </Grid>
+    </AuthForm>
+  );
+};
 
 export default SignUpPage;
