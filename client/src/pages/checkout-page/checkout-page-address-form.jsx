d360ea3334620cc632bcad341042b29eac00d754
Nojus Jankevičius <1nojus.jankevicius@gmail.com>
2022-02-02 13:59:22 +0200

    chore: deleted comments in checkout-page

--- client/src/pages/checkout-page/checkout-page-address-form.jsx
+++ client/src/pages/checkout-page/checkout-page-address-form.jsx
@@ -2,81 +2,70 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
 
-// eslint-disable-next-line arrow-body-style
-const CheckoutPageAddressForm = () => {
-  return (
-    <>
-      <Typography variant="h6" gutterBottom>
-        Užsakymo gavimo adresas
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="Vardas"
-            fullWidth
-            autoComplete="given-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Pavardė"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Adresas"
-            fullWidth
-            autoComplete="shipping address-line1"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="Miestas"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Pašto kodas"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          />
-        </Grid>
-        {/* <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
-        </Grid> */}
+const CheckoutPageAddressForm = () => (
+  <>
+    <Typography variant="h6" gutterBottom>
+      Užsakymo gavimo adresas
+    </Typography>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          required
+          id="firstName"
+          name="firstName"
+          label="Vardas"
+          fullWidth
+          autoComplete="given-name"
+          variant="standard"
+        />
       </Grid>
-    </>
-  );
-};
+      <Grid item xs={12} sm={6}>
+        <TextField
+          required
+          id="lastName"
+          name="lastName"
+          label="Pavardė"
+          fullWidth
+          autoComplete="family-name"
+          variant="standard"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          required
+          id="address1"
+          name="address1"
+          label="Adresas"
+          fullWidth
+          autoComplete="shipping address-line1"
+          variant="standard"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          required
+          id="city"
+          name="city"
+          label="Miestas"
+          fullWidth
+          autoComplete="shipping address-level2"
+          variant="standard"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          required
+          id="zip"
+          name="zip"
+          label="Pašto kodas"
+          fullWidth
+          autoComplete="shipping postal-code"
+          variant="standard"
+        />
+      </Grid>
+    </Grid>
+  </>
+);
 
 export default CheckoutPageAddressForm;
