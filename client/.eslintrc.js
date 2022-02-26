22d70181386cbfac5baa327dda59a77d3ac8cc78
Nojus Jankevičius <1nojus.jankevicius@gmail.com>
2022-02-26 18:15:51 +0200

    feat: eslint configured

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,26 +2,45 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/prop-types': 0,
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
-    'react/jsx-props-no-spreading': 0,
+    quotes: [2, 'single'],
+    semi: [2, 'always'],
     'quote-props': [2, 'as-needed'],
+    'linebreak-style': [2, 'unix'],
+    'no-multiple-empty-lines': [
+      2,
+      {
+        max: 1,
+        maxEOF: 0,
+      },
+    ],
+    'no-trailing-spaces': [
+      2,
+      {
+        skipBlankLines: true,
+        ignoreComments: true,
+      },
+    ],
+    'no-throw-literal': [2],
+    'prefer-promise-reject-errors': [2],
+    'eol-last': [2, 'unix'],
+    'newline-before-return': [2],
+    '@typescript-eslint/semi': [2, 'always'],
   },
 };
